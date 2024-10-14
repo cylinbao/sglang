dc18560a8729fd77331a416f2a0d24aed77ebc77
Chien-Yu Lin <cyulin@cs.washington.edu>
2024-10-14 18:37:31 +0000

    update

--- python/sglang/bench_overlap.py
+++ python/sglang/bench_overlap.py
@@ -51,11 +51,16 @@ import os
 import sqlite3
 import time
 from typing import Tuple
+import nvtx
 
 import numpy as np
 import pandas as pd
 import torch
 import torch.distributed as dist
+from torch.profiler import profile, record_function, ProfilerActivity
+
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import threading
 
 from sglang.srt.configs.model_config import ModelConfig
 from sglang.srt.hf_transformers_utils import get_tokenizer
@@ -143,46 +148,6 @@ def load_model(server_args, tp_rank):
     return model_runner, tokenizer
 
 
-def prepare_inputs_for_correctness_test(bench_args, tokenizer):
-    prompts = [
-        "The capital of France is",
-        "The capital of the United Kindom is",
-        "Today is a sunny day and I like",
-    ]
-    input_ids = [tokenizer.encode(p) for p in prompts]
-    sampling_params = SamplingParams(
-        temperature=0,
-        max_new_tokens=BenchArgs.output_len,
-    )
-
-    reqs = []
-    for i in range(len(prompts)):
-        assert len(input_ids[i]) > bench_args.cut_len
-
-        tmp_input_ids = input_ids[i][: bench_args.cut_len]
-        req = Req(rid=i, origin_input_text=prompts[i], origin_input_ids=tmp_input_ids)
-        req.prefix_indices = []
-        req.sampling_params = sampling_params
-        req.fill_ids = req.origin_input_ids
-        req.extend_input_len = len(req.fill_ids) - len(req.prefix_indices)
-        reqs.append(req)
-
-    return input_ids, reqs
-
-
-def prepare_extend_inputs_for_correctness_test(
-    bench_args, input_ids, reqs, model_runner
-):
-    for i in range(len(reqs)):
-        req = reqs[i]
-        req.fill_ids += input_ids[i][bench_args.cut_len :]
-        req.prefix_indices = model_runner.req_to_token_pool.req_to_token[
-            i, : bench_args.cut_len
-        ]
-        req.extend_input_len = len(req.fill_ids) - len(req.prefix_indices)
-    return reqs
-
-
 def prepare_synthetic_inputs_for_latency_test(batch_size, input_len):
     input_ids = np.ones((batch_size, input_len), dtype=np.int32)
     sampling_params = SamplingParams(
@@ -202,6 +167,7 @@ def prepare_synthetic_inputs_for_latency_test(batch_size, input_len):
     return reqs
 
 
+@nvtx.annotate(message="extend", color="blue")
 def extend(reqs, model_runner):
     batch = ScheduleBatch.init_new(
         reqs=reqs,
@@ -215,144 +181,130 @@ def extend(reqs, model_runner):
     return next_token_ids, logits_output.next_token_logits, batch
 
 
+@nvtx.annotate(message="decode", color="orange")
 def decode(input_token_ids, batch, model_runner):
     batch.prepare_for_decode(input_token_ids)
     logits_output = model_runner.forward(batch)
     next_token_ids = model_runner.sample(logits_output, batch).tolist()
     return next_token_ids, logits_output.next_token_logits
 
+@nvtx.annotate(message="copy_data", color="red")
+def copy_data(src_data, tar_data):
+    # tar_data.copy_(src_data, non_blocking=True)
 
-@torch.inference_mode()
-def correctness_test(
-    server_args,
-    bench_args,
-    tp_rank,
-):
-    rank_print = print if tp_rank == 0 else lambda *args, **kwargs: None
+    with torch.profiler.record_function("copy_data"):
+        # tar_data.copy_(src_data, non_blocking=True)
+        tar_data.copy_(src_data)
 
-    # Load the model
-    model_runner, tokenizer = load_model(server_args, tp_rank)
-
-    # Prepare inputs
-    input_ids, reqs = prepare_inputs_for_correctness_test(bench_args, tokenizer)
-    rank_print(f"\n{input_ids=}\n")
-
-    if bench_args.cut_len > 0:
-        # Prefill
-        next_token_ids, next_token_logits, batch = extend(reqs, model_runner)
-        rank_print(f"prefill logits (first half): {next_token_logits} \n")
-
-    # Prepare extend inputs
-    reqs = prepare_extend_inputs_for_correctness_test(
-        bench_args, input_ids, reqs, model_runner
-    )
-
-    # Extend
-    next_token_ids, next_token_logits, batch = extend(reqs, model_runner)
-    rank_print(f"prefill logits (final): {next_token_logits} \n")
-
-    # Decode
-    output_ids = [input_ids[i] + [next_token_ids[i]] for i in range(len(input_ids))]
-    for _ in range(bench_args.output_len[0]):
-        next_token_ids, _ = decode(next_token_ids, batch, model_runner)
-        for i in range(len(reqs)):
-            output_ids[i].append(next_token_ids[i])
-
-    # Print
-    for i in range(len(reqs)):
-        rank_print(f"========== Prompt {i} ==========")
-        rank_print(tokenizer.decode(output_ids[i]), "\n")
-
+    # with ThreadPoolExecutor() as executor:
+    #     copy = executor.submit(
+    #         tar_data.copy_, src_data
+    #     )
 
 @torch.inference_mode()
 def overlap_test_run_once(
     run_name, model_runner, rank_print, reqs, batch_size, input_len, output_len,
-    data_cpu, data_gpu
-    # data, device
+    data, data_pin, data_gpu, data2
 ):
-    max_batch_size = model_runner.max_total_num_tokens // (input_len + output_len)
-    if batch_size > max_batch_size:
-        rank_print(
-            f"skipping ({batch_size}, {input_len}, {output_len}) due to max batch size limit"
-        )
-        return
-
     # Clear the pools.
-    model_runner.req_to_token_pool.clear()
-    model_runner.token_to_kv_pool.clear()
+    # model_runner.req_to_token_pool.clear()
+    # model_runner.token_to_kv_pool.clear()
 
-    measurement_results = {
-        "run_name": run_name,
-        "batch_size": batch_size,
-        "input_len": input_len,
-        "output_len": output_len,
-    }
+    print("t0, data_pint[0,:10]: ", data_pin[0,:10])
+    tick = time.time()
+    copy_data(data, data_pin)
+    print("t1, data_pint[0,:10]: ", data_pin[0,:10])
+    print(f"Copy time: {time.time() - tick}")
 
-    # tot_latency = 0
-    s1 = torch.cuda.Stream()
-    s2 = torch.cuda.Stream()
+    tick = time.time()
+    data_pin[:] = 0
+    print(f"clear data_pin time: {time.time() - tick}")
+    print("t4, data_pint[0,:10]: ", data_pin[0,:10])
 
-    # Prefill
-    # torch.cuda.synchronize()
-    # tic = time.time()
-    with torch.cuda.stream(s2):
-        next_token_ids, _, batch = extend(reqs, model_runner)
-    # torch.cuda.synchronize()
-    # prefill_latency = time.time() - tic
-    # tot_latency += prefill_latency
-    # throughput = input_len * batch_size / prefill_latency
-    # rank_print(
-    #     f"Prefill. latency: {prefill_latency:6.5f} s, throughput: {throughput:9.2f} token/s"
-    # )
-    # measurement_results["prefill_latency"] = prefill_latency
-    # measurement_results["prefill_throughput"] = throughput
+    tick = time.time()
+    copy_data(data, data_pin)
+    print("t3, data_pint[0,:10]: ", data_pin[0,:10])
+    print(f"Copy time: {time.time() - tick}")
 
-    # with torch.cuda.stream(s1):
-    #     data = data.to(device=device, non_blocking=True)
-    # from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+    # tick = time.time()
+    # data_pin[:] = 0
+    # print(f"clear data_pin time: {time.time() - tick}")
+    # print("t2, data_pint[0,:10]: ", data_pin[0,:10])
+
+    # tick = time.time()
+    # copy_data(data2, data_pin)
+    # print("t3, data_pint[0,:10]: ", data_pin[0,:10])
+    # print(f"Copy time: {time.time() - tick}")
+
+    # tick = time.time()
+    # data_pin[:] = 0
+    # print(f"clear data_pin time: {time.time() - tick}")
+    # print("t4, data_pint[0,:10]: ", data_pin[0,:10])
+
+    # tick = time.time()
+    # copy_data(data2, data_pin)
+    # print("t3, data_pint[0,:10]: ", data_pin[0,:10])
+    # print(f"Copy time: {time.time() - tick}")
+
+    tick = time.time()
+    data_pin[:] = 0
+    print(f"clear data_pin time: {time.time() - tick}")
+    print("t4, data_pint[0,:10]: ", data_pin[0,:10])
+
+    # for i in range(5):
+    #     next_token_ids, _, batch = extend(reqs, model_runner)
+
+    # tick = time.time()
+    # next_token_ids, _, batch = extend(reqs, model_runner)
+    # # Decode
+    # for i in range(output_len):
+    #     # if i == 1:
+    #     #     with torch.cuda.stream(s1):
+    #     #         data_gpu.copy_(data_pin, non_blocking=True)
+
+    #     next_token_ids, _ = decode(next_token_ids, batch, model_runner)
+    # print(f"LLM time 1: {time.time() - tick}")
+
+    # tick = time.time()
+    # next_token_ids, _, batch = extend(reqs, model_runner)
+    # # Decode
+    # for i in range(output_len):
+    #     # if i == 1:
+    #     #     with torch.cuda.stream(s1):
+    #     #         data_gpu.copy_(data_pin, non_blocking=True)
+
+    #     next_token_ids, _ = decode(next_token_ids, batch, model_runner)
+    # print(f"LLM time 2: {time.time() - tick}")
+
+    # s1 = torch.cuda.Stream()
+
     # with ThreadPoolExecutor() as executor:
-    #     output0 = executor.submit(data.to, device, non_blocking=True)
+    #     copy = executor.submit(
+    #         copy_data, data, data_pin
+    #     )
+        # with torch.profiler.record_function("copy_data"):
+        #     copy = executor.submit(
+        #         copy_data, data, data_pin
+        #     )
 
+    # with torch.profiler.record_function("copy_data"):
+    #     copy_data(data, data_pin)
+
+    copy_thread = threading.Thread(target=copy_data, args=(data2, data_pin))
+    tick = time.time()
+    copy_thread.start()
+
+    next_token_ids, _, batch = extend(reqs, model_runner)
     # Decode
-    decode_latencies = []
-    with torch.cuda.stream(s2):
-        for i in range(output_len):
-            # torch.cuda.synchronize()
-            # tic = time.time()
+    for i in range(output_len):
+        next_token_ids, _ = decode(next_token_ids, batch, model_runner)
+    
+    # copy.result()
+    copy_thread.join()
+    print(f"Copy + LLM time: {time.time() - tick}")
+    print("t5, data_pint[0,:10]: ", data_pin[0,:10])
 
-            if i == 1:
-                with torch.cuda.stream(s1):
-                    # data.to(device=device, non_blocking=True)
-                    data_gpu.copy_(data_cpu, non_blocking=True)
-
-            next_token_ids, _ = decode(next_token_ids, batch, model_runner)
-            # torch.cuda.synchronize()
-            # latency = time.time() - tic
-            # tot_latency += latency
-            # throughput = batch_size / latency
-            # decode_latencies.append(latency)
-            # if i < 5:
-            #     rank_print(
-            #         f"Decode.  latency: {latency:6.5f} s, throughput: {throughput:9.2f} token/s"
-            #     )
-
-    # data.cpu().pin_memory()
-
-    # med_decode_latency = np.median(decode_latencies)
-    # med_decode_throughput = batch_size / med_decode_latency
-    # rank_print(
-    #     f"Decode.  median latency: {med_decode_latency:6.5f} s, median throughput: {med_decode_throughput:9.2f} token/s"
-    # )
-    # measurement_results["median_decode_latency"] = med_decode_latency
-    # measurement_results["median_decode_throughput"] = med_decode_throughput
-
-    # throughput = (input_len + output_len) * batch_size / tot_latency
-    # rank_print(
-    #     f"Total. latency: {tot_latency:6.3f} s, throughput: {throughput:9.2f} token/s"
-    # )
-    # measurement_results["total_latency"] = tot_latency
-    # measurement_results["total_throughput"] = throughput
-    return measurement_results
+    return
 
 
 def overlap_test(
@@ -360,12 +312,16 @@ def overlap_test(
     bench_args,
     tp_rank,
 ):
+    print("Overlap Test!")
     rank_print = print if tp_rank == 0 else lambda *args, **kwargs: None
 
-    n_data = 1024*1024
+    n_data = 2*1024*1024
     h_dim = 768
 
     device = torch.device(f"cuda:{tp_rank}")
+    # data = torch.ones(n_data, h_dim, dtype=torch.float16)
+    data = torch.rand(n_data, h_dim, dtype=torch.float16)
+    data2 = torch.rand(n_data, h_dim, dtype=torch.float16)
     data_cpu = torch.zeros(n_data, h_dim, dtype=torch.float16).pin_memory()
     data_gpu = torch.empty(n_data, h_dim, dtype=torch.float16, device=device)
 
@@ -377,50 +333,47 @@ def overlap_test(
         bench_args.batch_size[0], bench_args.input_len[0]
     )
 
-    # Warm up
-    rank_print("Warmup ...")
-    overlap_test_run_once(
-        bench_args.run_name,
-        model_runner,
-        rank_print,
-        reqs,
-        bench_args.batch_size[0],
-        bench_args.input_len[0],
-        # 4,  # shorter decoding to speed up the warmup
-        bench_args.output_len[0],  # shorter decoding to speed up the warmup
-        data_cpu,
-        data_gpu,
-        # device
-    )
-    rank_print("Benchmark ...")
+    with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], 
+             record_shapes=True, 
+             profile_memory=True) as prof:
+        # Warm up
+        # rank_print("Warmup ...")
+        overlap_test_run_once(
+            bench_args.run_name,
+            model_runner,
+            rank_print,
+            reqs,
+            bench_args.batch_size[0],
+            bench_args.input_len[0],
+            bench_args.output_len[0],
+            data,
+            data_cpu,
+            data_gpu,
+            data2
+        )
+    # overlap_test_run_once(
+    #     bench_args.run_name,
+    #     model_runner,
+    #     rank_print,
+    #     reqs,
+    #     bench_args.batch_size[0],
+    #     bench_args.input_len[0],
+    #     bench_args.output_len[0],
+    #     data,
+    #     data_cpu,
+    #     data_gpu,
+    # )
 
-    # Run the sweep
-    # result_list = []
-    # for bs, il, ol in itertools.product(
-    #     bench_args.batch_size, bench_args.input_len, bench_args.output_len
-    # ):
-    #     reqs = prepare_synthetic_inputs_for_latency_test(bs, il)
-    #     ret = latency_test_run_once(
-    #         bench_args.run_name, model_runner, rank_print, reqs, bs, il, ol
-    #     )
-    #     if ret is not None:
-    #         result_list.append(ret)
-
-    # # Write results in jsonlines format on rank 0.
-    # if tp_rank == 0 and bench_args.result_filename:
-    #     import jsonlines
-
-    #     with jsonlines.open(bench_args.result_filename, "a") as f:
-    #         f.write_all(result_list)
+    torch.cuda.synchronize()
+    # print(prof.key_averages().table())
+    prof.export_chrome_trace("./overlap_trace/pytorch_trace_v2.6.json")
+    # rank_print("Benchmark ...")
+    print("Finished!!")
 
 
 def main(server_args, bench_args):
-
     if server_args.model_path:
-        if bench_args.correctness_test:
-            work_func = correctness_test
-        else:
-            work_func = overlap_test
+        work_func = overlap_test
     else:
         raise ValueError(
             "Provide --model-path for running the tests or "
